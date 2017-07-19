c9325754758bd54cdc33464ccf0b3e63997d6640
Douglas Serbino <douglas@serbino>
2017-07-18 23:51:39 -0600

    Controladores Reportes

--- src/Minsal/CoreBundle/Controller/ConsumoMensualController.php
+++ src/Minsal/CoreBundle/Controller/ConsumoMensualController.php
@@ -5,10 +5,6 @@ namespace Minsal\CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class ReporteController extends Controller
+class ConsumoMensualController extends Controller
 {
-	public function indexAction()
-	{
-		return $this->render('MinsalPlantillaBundle:Reporte:inicio.html.twig');
-	}
 }
---
+++ src/Minsal/CoreBundle/Controller/ReporteMapaAppController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Minsal\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ReporteMapaAppController extends Controller
+{
+}
