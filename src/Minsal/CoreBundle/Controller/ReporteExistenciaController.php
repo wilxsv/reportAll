2406c7e46ed035f95f538dd5e9ebe8d78d4fe156
Douglas Serbino <douglas@serbino>
2017-07-19 00:56:38 -0600

    Cambios

--- src/Minsal/CoreBundle/Controller/ReporteExistenciaController.php
+++ src/Minsal/CoreBundle/Controller/ReporteExistenciaController.php
@@ -7,4 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ReporteExistenciaController extends Controller
 {
+	public function formularioExistenciaAction()
+	{
+		return $this->render('MinsalPlantillaBundle:Reporte:reporteExistencia.html.twig');
+	}
 }
